7e1aed447578684ca5bad26d1058c6cffdb173e9
Rishav Chakravarty <rishavchakravarty18@gmail.com>
2025-09-13 22:13:04 -0400

    Performance updates

--- next.config.js
+++ next.config.js
@@ -1,19 +1,7 @@
 /** @type {import('next').NextConfig} */
-const isGitHubPages = process.env.GITHUB_PAGES === 'true'; // set only during deploy
-const repo = 'portfolio-rishav'; // <-- your repository name
-
-export default {
-  // Produce a static site 
+module.exports = {
   output: 'export',
-
-  // Next/Image on static hosts 
   images: { unoptimized: true },
-
-  // Avoid 404s by ensuring folders like /projects/ map to /projects/index.html
   trailingSlash: true,
-
-  // Only set basePath/assetPrefix for GitHub Pages *project* sites
-  // (username.github.io/<repo>)
-  basePath: isGitHubPages ? `/${repo}` : '',
-  assetPrefix: isGitHubPages ? `/${repo}/` : '',
+  basePath: '',
 };
